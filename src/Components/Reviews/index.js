1f88e5fb3c3ee00c41002d4348d78025c4a51c55
OhDangBro <103394466+OhDangBro@users.noreply.github.com>
2023-02-20 21:34:04 -0500

    added google reviews, with capital first letter for name function

--- src/Components/Reviews/index.js
+++ src/Components/Reviews/index.js
@@ -1,75 +1,55 @@
-import * as React from 'react';
-import { Container } from '@mui/system';
-import Carousel from 'react-bootstrap/Carousel';
+import React, { useState, useEffect } from "react";
+import { Container } from "@mui/system";
+import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 
+function getStarRating(rating) {
+  let stars = '';
+  for (let i = 0; i < rating; i++) {
+    stars += '⭐️';
+  }
+  return stars;
+}
 
-const FAQSections = [
-  {
-    rating: "⭐⭐⭐⭐⭐",
-    description: `There are no words to describe my experiences with Christina. She is truly amazing! After
-    every session I have had with her, I left feeling relaxed, refreshed and inspired. She has
-    the most beautiful space as well! You will feel relaxed as soon as you enter. If you are
-    looking for mental and/or physical holistic healing, this is your girl! Just being in her
-    presence and talking with her is positive energy. 10 out of 10!`,
-    name: "Allie DeAngelis"
-  },
-  {
-    rating: "⭐⭐⭐⭐⭐",
-    description:
-      'I have been working with Christina for over 3 years. Her gifts are some of the most unique and remarkable skill I have ever had the pleasure of experiencing. Every session she makes you feel that you have entered a safe and open place where you can just release. The infrared sauna service is a superb touch to heighten the powerful session. She really brings you deep into relaxation, to your own self, and to the most magical places! I highly recommend her services to anyone and everyone! She is truly and honestly one of a kind ✨',
-    name: "Taylor Gallo"
-  },
-  {
-    rating: "⭐⭐⭐⭐⭐",
-    description: `Christina's innate ability to connect with you is magical. She can sense your energy before you
-    walk through the door and is equipped to provide healing and clarity that's best suited to your
-    situation. From the moment you book your appointment to the frequent check-ins afterward - your
-    time is efficiently spent. She is a consummate professional and most importantly - she cares
-    about your well being. Highly recommend!`,
-    name: "Suzette Minetti"
-  },
-  {
-    rating: "⭐⭐⭐⭐⭐",
-    description: `I was very stressed and anxious before my wedding. I saw Christina for reiki about a week and a
-    half before. I had never done something like this but I am so happy I did!! Christina is an
-    amazing person, a good listener, and gives great advice. I felt so calm and collected after my
-    session and for the remaining week leading up to my wedding. I am so grateful for my experience
-    with Christina and highly recommend booking a session with her, you won’t regret it!`,
-    name: "Allie DeAngelis"
-  },
-  {
-    rating: "⭐⭐⭐⭐⭐",
-    description: 'I feel so eternally blessed to have Christina as friend and healer! I’ve been attending reiki sessions with Christina for years and I will never go to anyone else. She truly cares about you as a person and will guide you as much as you require. She has a true gift and is a beautiful person inside and out. I feel safe, relaxed, and at peace when I am in her presence. I recommend her always!!',
-    name: "Nicolette DeAngelis"
-  },
-  {
-    rating: "⭐⭐⭐⭐⭐",
-    description: `I’ve been coming to Christina over the last few years for energy healing. She has allowed me to
-    awaken my intuition and deepen my relationship with my self. She has the ability to hold this
-    beautiful, safe and intimate space to explore these depths. Not only do you receive reiki but
-    the intuitive guidance and thought provoking questions she asks feels like a therapy session
-    too! Highly recommend!`,
-    name: "Danielle Brown"
-  },
-];
+function capitalizeWords(str) {
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
 
 export default function Reviews() {
-return (
-    <section id="Reviews"> 
-      <h2 className='gellatio-font'> Reviews</h2>
-<Carousel>
-{FAQSections.map(section =>
-      <Carousel.Item interval={5000}>
-        <Container className="reviewContainer">
-        <h3>{section.rating}</h3>
-        <h3 className="lead">{section.description}</h3>
-        <p>{section.name}</p>
-        </Container>
-      </Carousel.Item>
-)}
-    </Carousel>
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const placeId = "ChIJcUQQF1CBwokRB-zz3K0pCNo";
+    const apiKey = "AIzaSyDkK7xOGH20-Oc5hIoFBCgIds7EnYoQzqs";
+    const url = `/api/place/details/json?place_id=${placeId}&fields=reviews&key=${apiKey}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const filteredReviews = data.result.reviews.filter((review) => review.text);
+        setReviews(filteredReviews);
+      });
+  }, []);
+
+  return (
+    <section id="Reviews">
+      <h2 className="gellatio-font">Reviews</h2>
+      <Carousel>
+        {reviews.map((review) => (
+          <Carousel.Item interval={5000}>
+          <Container className="reviewContainer">
+  <h3>{getStarRating(review.rating)}</h3>
+  <h3 className="lead">{review.text}</h3>
+  <p>{capitalizeWords(review.author_name)}</p>
+</Container>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </section>
   );
 }
